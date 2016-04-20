15f3f068a0ee655ad0dfeb615e0b40e6f9800854
kalsowerus <roy.geesing@hispeed.ch>
2016-04-20 16:13:57 +0200

    moved starter to different package

--- src/jaws/start/Starter.java
+++ src/jaws/start/Starter.java
@@ -1,4 +1,5 @@
-package jaws.business.start;
+package jaws.start;
+
 import jaws.business.init.WebInitializer;
 
 public class Starter {
@@ -6,7 +7,7 @@ public class Starter {
 	private static final String configFile = "../config";
 
 	public static void main(String[] args) {
-		
+
 		WebInitializer.init(configFile);
 	}
 }
