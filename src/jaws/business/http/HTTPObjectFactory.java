237adf5c5888b1177f7f61701ee510eee52e849a
overactor <rik.degraaff@gmx.ch>
2016-06-08 20:23:31 +0200

    added JavaDocs to HTTPObjectFactory

--- src/jaws/business/http/HTTPObjectFactory.java
+++ src/jaws/business/http/HTTPObjectFactory.java
@@ -6,18 +6,41 @@ import java.io.IOException;
 import jaws.module.http.HTTPRequest;
 import jaws.module.http.HTTPResponse;
 
-public class HTTPObjectFactory {
-
+/**
+ * A static class that allows the creation of HTTP Objects
+ * 
+ * @author Rik
+ *
+ */
+final public class HTTPObjectFactory {
+	
+	private HTTPObjectFactory() {}
+	
+	/**
+	 * Creates an empty HTTP request.
+	 * 
+	 * @return the HTTP request.
+	 */
 	public static HTTPRequest createRequest() {
 
 		return new DefaultHTTPRequest();
 	}
 
+	/**
+	 * Creates a HTTP request by parsing a BufferedReader.
+	 * 
+	 * @return the parsed HTTP request.
+	 */
 	public static HTTPRequest parseRequest(BufferedReader reader) throws IOException {
 
 		return DefaultHTTPRequest.parse(reader);
 	}
 
+	/**
+	 * Creates an empty HTTP response.
+	 * 
+	 * @return the HTTP response.
+	 */
 	public static HTTPResponse createResponse() {
 
 		return new DefaultHTTPResponse();
